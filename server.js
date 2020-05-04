0c4ff0bd454b91ab56573417e403e198002043ac
EC2 Default User <ec2-user@ip-172-31-81-169.ec2.internal>
2020-05-04 16:30:25 +0000

    develop branch added

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require('express');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 var app = express();
 
 app.get('/', function(req, res) {
   res.send({
-    "Output" : "Hello World!"
+    "Output" : "Hello GET REQUEST World!"
   });
 });
 
 app.post('/', function(req, res) {
   res.send({
-    "Output" : "Hello World!"
+    "Output" : "Hello POST REQUEST World!"
   });
 });
 
